19725276c7e851782a53508e231b38ee6530cf9c
topherwalker <git@christopherwalker.tech>
2024-03-14 20:32:00 +0000

    fix: changed name of vairable that was changed in .env

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ dotenv.config({
 	path: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
 });
 export const CONFIG = {
-     port: process.env.PORT ?? 3000,
+     port: process.env.SERVICE_PORT ?? 3000,
      dbName: process.env.DB_NAME ?? "sqlite::memory:",
      dbUserName: process.env.DB_USERNAME ?? "",
      dbPassword: process.env.DB_PASSWORD ?? "",
