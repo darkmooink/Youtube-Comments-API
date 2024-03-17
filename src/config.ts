fcf67324d1d634ab02badc2014968128348d17ae
Chae Cramb <chaecramb@gmail.com>
2024-03-17 16:24:11 +0000

    revert autoformat

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 import * as dotenv from "dotenv";
-
 dotenv.config({
-  path: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+	path: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
 });
 export const CONFIG = {
-  port: process.env.SERVICE_PORT ?? 3000,
-  dbName: process.env.DB_NAME ?? "sqlite::memory:",
-  dbUserName: process.env.DB_USERNAME ?? "",
-  dbPassword: process.env.DB_PASSWORD ?? "",
-  dbHost: process.env.DB_HOST ?? "localhost",
-  dbDialect: process.env.DB_DIALECT ?? "sqlite",
+     port: process.env.SERVICE_PORT ?? 3000,
+     dbName: process.env.DB_NAME ?? "sqlite::memory:",
+     dbUserName: process.env.DB_USERNAME ?? "",
+     dbPassword: process.env.DB_PASSWORD ?? "",
+     dbHost: process.env.DB_HOST ?? "localhost",
+     dbDialect: process.env.DB_DIALECT ?? "sqlite",
 } as const;
