30d07ddc6e3dcaf28859e932027805c6a853350d
Chae Cramb <chaecramb@gmail.com>
2024-03-18 13:20:49 +0000

    feat: extends comment fields

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -1,85 +1,110 @@
 import {
-  Model,
-  InferAttributes,
-  InferCreationAttributes,
-  DataTypes,
-  HasManyGetAssociationsMixin,
-  HasManyAddAssociationMixin,
-  HasManyHasAssociationMixin,
-  Association,
-} from "sequelize";
-import { sequelize } from "../database/database";
+    Model,
+    InferAttributes,
+    InferCreationAttributes,
+    DataTypes,
+    HasManyGetAssociationsMixin,
+    HasManyAddAssociationMixin,
+    HasManyHasAssociationMixin,
+    Association,
+} from 'sequelize'
+import { sequelize } from '../database/database'
 
 export class Comment extends Model<
-  InferAttributes<Comment>,
-  InferCreationAttributes<Comment>
+    InferAttributes<Comment>,
+    InferCreationAttributes<Comment>
 > {
-  declare id: string;
-  declare parentId: string | null;
-  declare channelId: string;
-  declare videoId: string;
-  declare author: string;
-  declare likeCount: number;
-  declare text: string;
-  declare getReplies: HasManyGetAssociationsMixin<Comment>;
-  declare addReply: HasManyAddAssociationMixin<Comment, string>;
-  declare hasReply: HasManyHasAssociationMixin<Comment, string>;
+    declare id: string
+    declare parentId: string | null
+    declare channelId: string
+    declare videoId: string
+    declare author: string
+    declare likeCount: number
+    declare text: string
+    declare sentiment: number | null
+    declare timeSubmitted: Date
+    declare timeArchived: Date
+    declare authorChannelId: string
+    declare authorChannelUrl: string
+    declare getReplies: HasManyGetAssociationsMixin<Comment>
+    declare addReply: HasManyAddAssociationMixin<Comment, string>
+    declare hasReply: HasManyHasAssociationMixin<Comment, string>
 
-  declare static associations: {
-    replies: Association<Comment, Comment>;
-  };
+    declare static associations: {
+        replies: Association<Comment, Comment>
+    }
 }
 
 Comment.init(
-  {
-    id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      primaryKey: true,
+    {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+        },
+        parentId: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            references: {
+                model: 'Comments',
+                key: 'id',
+            },
+        },
+        channelId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        videoId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        likeCount: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        sentiment: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        timeSubmitted: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        timeArchived: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        authorChannelId: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        authorChannelUrl: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
-    parentId: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      references: {
-        model: "Comments",
-        key: "id",
-      },
+    {
+        modelName: 'Comment',
+        tableName: 'Comments',
+        timestamps: false,
+        sequelize: sequelize,
     },
-    channelId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    videoId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    author: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    likeCount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    modelName: "Comment",
-    tableName: "Comments",
-    timestamps: false,
-    sequelize: sequelize,
-  }
-);
+)
 
 Comment.hasMany(Comment, {
-  as: "replies",
-  foreignKey: "parentId",
-});
+    as: 'replies',
+    foreignKey: 'parentId',
+})
 
 Comment.belongsTo(Comment, {
-  as: "parent",
-  foreignKey: "parentId",
-});
+    as: 'parent',
+    foreignKey: 'parentId',
+})
--- src/types/comment.d.ts
+++ src/types/comment.d.ts
@@ -1,10 +1,15 @@
 export type CommentData = {
-  id: string;
-  parentId: string | null;
-  channelId: string;
-  videoId: string;
-  author: string;
-  likeCount: number;
-  text: string;
-  replies?: CommentData[];
-};
+    id: string
+    parentId: string | null
+    channelId: string
+    videoId: string
+    author: string
+    likeCount: number
+    text: string
+    replies?: CommentData[]
+    sentiment: number | null
+    timeSubmitted: Date
+    timeArchived: Date
+    authorChannelId: string
+    authorChannelUrl: string
+}
