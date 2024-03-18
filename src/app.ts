6630ef261bf9b57678b10220a50c0b66874a48a1
Helen Pickavance <ellohez@gmail.com>
2024-03-18 22:09:13 +0000

    feat: swagger config and file hooked up

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
 import express from 'express'
 import { router } from './routes/routes'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 export const app = express()
+
+const swaggerConfiguration = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'YouTube Comment Analysis service',
+            version: '0.1',
+            description:
+                'Need to analyse comments on a YouTube video to determine the tone, content and identify spam?' +
+                '',
+        },
+    },
+    apis: ['src/routes/routes.js', 'swagger-config.yaml'],
+}
+const specs = swaggerJSDoc(swaggerConfiguration)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 app.use(express.json())
 
 app.get('/', (req, res) => res.send('Welcome to YouTube Comment Analysis API!'))
 
 app.use('/', router)
+
+
