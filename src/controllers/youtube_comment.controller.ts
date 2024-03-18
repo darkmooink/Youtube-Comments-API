fdd0f30cd76a231218ee1d009ac8d99163bdadad
Debola Adedapo <niikee80@hotmail.com>
2024-03-18 16:37:53 +0000

    feat: data formated in a better format as array of comments-ISnippet

--- src/controllers/youtube_comment.controller.ts
+++ src/controllers/youtube_comment.controller.ts
@@ -3,9 +3,9 @@ import * as commentService from '../services/youtube_comment.service'
 
 export const getAllYoutubeComments = async (req: Request, res: Response) => {
     const commentList = await commentService.getYoutubeVideoComments(
-        'QZ4BXGgmATU',
+        'dQw4w9WgXcQ',
         1,
-        1,
+        100,
     )
     // res.json(commentList).status(200)
     res.send(commentList)
--- src/types/comment.d.ts
+++ src/types/comment.d.ts
@@ -13,3 +13,41 @@ export type CommentData = {
     authorChannelId: string
     authorChannelUrl: string
 }
+
+export type CommentItem = {
+    kind: string
+    etag: string
+    id: string
+    snippet: ISnippet
+    replies: Replies
+}
+export interface Comment {
+    kind: string
+    etag: string
+    id: string
+    snippet: ISnippet
+}
+
+export interface ISnippet {
+    channelId: string
+    videoId: string
+    textDisplay: string
+    textOriginal: string
+    parentId: string
+    authorDisplayName: string
+    authorProfileImageUrl: string
+    authorChannelUrl: string
+    authorChannelId: Value
+    canRate: boolean
+    viewerRating: string
+    likeCount: number
+    publishedAt: string
+    updatedAt: string
+    totalReplyCount: number
+    isPublic: boolean
+    canReply: boolean
+    topLevelComment: Comment
+}
+
+export type Value = { value: string }
+export type Replies = { comments: ISnippet[] }
