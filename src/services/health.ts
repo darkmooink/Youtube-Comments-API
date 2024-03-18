62e8bbf48f974fcf050a6f7811f48fe34ac78ad8
Helen Pickavance <ellohez@gmail.com>
2024-03-18 17:52:03 +0000

    refactor: types moved to their own file getYouTubeStatus moved from controller

--- src/services/health.ts
+++ src/services/health.ts
@@ -1,33 +1,41 @@
 import process from "process";
 import { getComments } from '../services/comments'
 import { Comment } from '../models/comment'
-
-export type serverStatus = {
-    uptime: number
-    responseTime: BigInt
-}
+import { getYoutubeVideoComments } from '../services/youtube_comment.service'
+import { serverStatus } from '../types/health'
 
 // TODO: Make return value either serverStatus or error
 export const getServerStatus = (): serverStatus => {
-    const serverUptime = process.uptime()
+    let serverUptime = 0
+    let benchmark = BigInt(0)
+
+    serverUptime = process.uptime()
     // High resolution real time in nanoseconds (BIGINT)
     const start = process.hrtime.bigint()
     let end = start
     setTimeout(() => {
         end = process.hrtime.bigint()
     }, 1000)
-    const benchmark = start === end ? start : end - start
+    benchmark = start === end ? start : end - start
 
     return { uptime: serverUptime, responseTime: benchmark }
 }
 
 export const getDatabaseStatus = async () => {
     const res: Comment[] = await getComments()
-    console.log('res :>> ', res)
 
     if (res.length === 0) {
         return false
     }
 
+    return true
+}
+
+export const getYouTubeStatus = async () => {
+    // TODO: There needs to be an error return value from this function.
+    const result = await getYoutubeVideoComments()
+    if (result === null) {
+        return false
+    }
     return true
 }
