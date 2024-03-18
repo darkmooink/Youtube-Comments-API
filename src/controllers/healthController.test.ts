f4cc9dafc1a16d235d96659efc6ca872c2f05b80
Helen Pickavance <ellohez@gmail.com>
2024-03-18 18:51:32 +0000

    test: refactored tests to test health endpoint now that code is hooked up

--- src/controllers/healthController.test.ts
+++ src/controllers/healthController.test.ts
@@ -1,82 +1,84 @@
-// import * as healthController from './health_controller'
-// import * as healthService from '../services/health'
-// import { serverStatus } from '../types/health'
+import request from 'supertest'
+import { app } from '../app'
+import * as healthService from '../services/health'
+import { serverStatus } from '../types/health'
+import { baseUrl } from '../routes/routes'
 
-// jest.mock('../services/health')
-// jest.mock('../services/youtube_comment.service')
+jest.mock('../services/health')
+jest.mock('../services/youtube_comment.service')
 
-// afterEach(() => {
-//     jest.clearAllMocks()
-// })
+afterEach(() => {
+    jest.clearAllMocks()
+})
 
-// describe('getHealthStatus()', () => {
-//     test('Returns healthy response if all services running', async () => {
-//         // Arrange
-//         jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
-//             uptime: 5.899,
-//             responseTime: BigInt(Number.MAX_SAFE_INTEGER),
-//         } as serverStatus)
-//         jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(true)
-//         jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(true)
+describe('getHealthStatus()', () => {
+    test('Returns healthy response if all services running', async () => {
+        // Arrange
+        jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
+            uptime: 5.899,
+            responseTime: BigInt(Number.MAX_SAFE_INTEGER),
+        } as serverStatus)
+        jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(true)
+        jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(true)
 
-//         // Act
-//         const healthStatus = await healthController.getHealthStatus()
-//         // Assert
-//         expect(healthStatus.status).toEqual('Ok')
-//         expect(healthStatus.server.status).toEqual('Ok')
-//         expect(healthStatus.database.status).toEqual('Ok')
-//         expect(healthStatus.youTube.status).toEqual('Ok')
-//     })
+        // Act
+        const res = await request(app).get(`${baseUrl}/health`)
+        // Assert
+        expect(res.body.status).toEqual('Ok')
+        expect(res.body.server.status).toEqual('Ok')
+        expect(res.body.database.status).toEqual('Ok')
+        expect(res.body.youTube.status).toEqual('Ok')
+    })
 
-//     test('Returns Not Ok response if server is not responding', async () => {
-//         // Arrange
-//         jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
-//             uptime: 0,
-//             responseTime: BigInt(0),
-//         } as serverStatus)
-//         jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(true)
-//         jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(true)
+    test('Returns Not Ok response if server is not responding', async () => {
+        // Arrange
+        jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
+            uptime: 0,
+            responseTime: BigInt(0),
+        } as serverStatus)
+        jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(true)
+        jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(true)
 
-//         // Act
-//         const healthStatus = await healthController.getHealthStatus()
-//         // Assert
-//         expect(healthStatus.status).toEqual('Not Ok')
-//         expect(healthStatus.server.status).toEqual('Not Ok')
-//         expect(healthStatus.database.status).toEqual('Ok')
-//         expect(healthStatus.youTube.status).toEqual('Ok')
-//     })
-//     test('Returns Not Ok response if server is not responding', async () => {
-//         // Arrange
-//         jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
-//             uptime: 5.899,
-//             responseTime: BigInt(Number.MAX_SAFE_INTEGER),
-//         } as serverStatus)
-//         jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(true)
-//         jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(false)
+        // Act
+        const res = await request(app).get(`${baseUrl}/health`)
+        // Assert
+        expect(res.body.status).toEqual('Not Ok')
+        expect(res.body.server.status).toEqual('Not Ok')
+        expect(res.body.database.status).toEqual('Ok')
+        expect(res.body.youTube.status).toEqual('Ok')
+    })
+    test('Returns Not Ok response if server is not responding', async () => {
+        // Arrange
+        jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
+            uptime: 5.899,
+            responseTime: BigInt(Number.MAX_SAFE_INTEGER),
+        } as serverStatus)
+        jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(true)
+        jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(false)
 
-//         // Act
-//         const healthStatus = await healthController.getHealthStatus()
-//         // Assert
-//         expect(healthStatus.status).toEqual('Not Ok')
-//         expect(healthStatus.server.status).toEqual('Ok')
-//         expect(healthStatus.database.status).toEqual('Not Ok')
-//         expect(healthStatus.youTube.status).toEqual('Ok')
-//     })
-//     test('Returns Not Ok response if YouTube API is not responding', async () => {
-//         // Arrange
-//         jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
-//             uptime: 5.899,
-//             responseTime: BigInt(Number.MAX_SAFE_INTEGER),
-//         } as serverStatus)
-//         jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(false)
-//         jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(true)
+        // Act
+        const res = await request(app).get(`${baseUrl}/health`)
+        // Assert
+        expect(res.body.status).toEqual('Not Ok')
+        expect(res.body.server.status).toEqual('Ok')
+        expect(res.body.database.status).toEqual('Not Ok')
+        expect(res.body.youTube.status).toEqual('Ok')
+    })
+    test('Returns Not Ok response if YouTube API is not responding', async () => {
+        // Arrange
+        jest.spyOn(healthService, 'getServerStatus').mockReturnValue({
+            uptime: 5.899,
+            responseTime: BigInt(Number.MAX_SAFE_INTEGER),
+        } as serverStatus)
+        jest.spyOn(healthService, 'getYouTubeStatus').mockResolvedValue(false)
+        jest.spyOn(healthService, 'getDatabaseStatus').mockResolvedValue(true)
 
-//         // Act
-//         const healthStatus = await healthController.getHealthStatus()
-//         // Assert
-//         expect(healthStatus.status).toEqual('Not Ok')
-//         expect(healthStatus.server.status).toEqual('Ok')
-//         expect(healthStatus.database.status).toEqual('Ok')
-//         expect(healthStatus.youTube.status).toEqual('Not Ok')
-//     })
-// })
+        // Act
+        const res = await request(app).get(`${baseUrl}/health`)
+        // Assert
+        expect(res.body.status).toEqual('Not Ok')
+        expect(res.body.server.status).toEqual('Ok')
+        expect(res.body.database.status).toEqual('Ok')
+        expect(res.body.youTube.status).toEqual('Not Ok')
+    })
+})
