3af3977f651c931ee73f22daf2d8bd13c8b829af
Helen Pickavance <ellohez@gmail.com>
2024-03-18 11:50:39 +0000

    fix: post merge fixes

--- src/services/health.test.ts
+++ src/services/health.test.ts
@@ -2,9 +2,10 @@ import * as healthService from "../services/health";
 
 describe("getServerStatus", () => {
   test("should return values greater than zero if server is running", () => {
-    // Act
-    const serverStatus = healthService.getServerStatus();
-    // Assert
-    expect(serverStatus.uptime).toBeGreaterThan(0.0);
+      // Act
+      const serverStatus = healthService.getServerStatus()
+      // Assert
+      expect(serverStatus.uptime).toBeGreaterThan(0.0)
   });
+
 });
