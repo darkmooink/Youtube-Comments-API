a1d3bc1a1335e6f705ccdfad4b9fced018370ade
Debola Adedapo <niikee80@hotmail.com>
2024-03-19 11:04:24 +0000

    getallyoutubecomments updated

--- src/controllers/youtube_comment.controller.ts
+++ src/controllers/youtube_comment.controller.ts
@@ -3,7 +3,15 @@ import * as commentService from '../services/youtube_comment.service'
 
 export const getAllYoutubeComments = async (req: Request, res: Response) => {
     const videoId = req.params.id
+    if (typeof videoId !== 'string')
+        res.status(400).json({ message: 'Invalid YoutubeId Id' })
+
     const maxResults = parseInt(req.params.maxResults)
+    if (Number.isNaN(maxResults) || !maxResults)
+        res.status(400).json({
+            message: 'Invalid Max Results value - it must be a number',
+        })
+
     const commentList = await commentService.getYoutubeVideoComments(
         videoId,
         1,
