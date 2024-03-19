d0431a2a75c3987c71863e2e537c936a377dbda2
Chae Cramb <chaecramb@gmail.com>
2024-03-19 11:21:49 +0000

    feat: save comments

--- src/controllers/youtube_comment.controller.ts
+++ src/controllers/youtube_comment.controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from 'express'
-import * as commentService from '../services/youtube_comment.service'
+import * as youtubeCommentService from '../services/youtube_comment.service'
+import * as commentService from '../services/comments'
+import { CommentData } from '../types/comment'
+import { parseYouTubeComments } from '../helpers/youtube_comment.helper'
 
 export const getAllYoutubeComments = async (req: Request, res: Response) => {
-    const commentList = await commentService.getYoutubeVideoComments(
+    const commentJson = await youtubeCommentService.getYoutubeVideoComments(
         'QZ4BXGgmATU',
         1,
-        1,
+        100,
     )
-    // res.json(commentList).status(200)
-    res.send(commentList)
+
+    try {
+        const comments: CommentData[] = parseYouTubeComments(commentJson)
+        comments.forEach((comment) =>
+            commentService.saveCommentWithReplies(comment),
+        )
+    } catch (e) {
+        console.error(e)
+    }
+
+    res.send(commentJson)
 }
--- src/helpers/youtube_comment.helper.ts
+++ src/helpers/youtube_comment.helper.ts
@@ -1,4 +1,7 @@
 import { CONFIG } from './../config'
+import { CommentListResponseSchema } from '../helpers/youtube_api_schema.helper'
+import { CommentData } from '../types/comment'
+
 const url = require('node:url')
 
 export const buildRequestUrl = (
@@ -18,4 +21,52 @@ export const buildRequestUrl = (
         },
     })
     return requestUrl
+}
+
+export const parseYouTubeComments = (commentJson: object) => {
+    const validatedCommentJson = CommentListResponseSchema.parse(commentJson)
+
+    const items = validatedCommentJson.items || []
+    const comments = items.map((item) => {
+        const topLevelComment = item.snippet.topLevelComment
+
+        const commentData: CommentData = {
+            id: topLevelComment.id,
+            parentId: null,
+            channelId: topLevelComment.snippet.channelId,
+            videoId: topLevelComment.snippet.videoId,
+            author: topLevelComment.snippet.authorDisplayName,
+            likeCount: topLevelComment.snippet.likeCount,
+            text: topLevelComment.snippet.textDisplay,
+            sentiment: null,
+            timeSubmitted: new Date(topLevelComment.snippet.publishedAt),
+            timeArchived: new Date(topLevelComment.snippet.updatedAt),
+            authorChannelId: topLevelComment.snippet.authorChannelId.value,
+            authorChannelUrl: topLevelComment.snippet.authorChannelUrl,
+        }
+
+        if (item.replies) {
+            commentData.replies = []
+            item.replies.comments.forEach((reply) => {
+                commentData.replies?.push({
+                    id: reply.id,
+                    parentId: commentData.id,
+                    channelId: reply.snippet.channelId,
+                    videoId: reply.snippet.videoId,
+                    author: reply.snippet.authorDisplayName,
+                    likeCount: reply.snippet.likeCount,
+                    text: reply.snippet.textDisplay,
+                    sentiment: null,
+                    timeSubmitted: new Date(reply.snippet.publishedAt),
+                    timeArchived: new Date(reply.snippet.updatedAt),
+                    authorChannelId: reply.snippet.authorChannelId.value,
+                    authorChannelUrl: reply.snippet.authorChannelUrl,
+                })
+            })
+        }
+
+        return commentData
+    })
+
+    return comments
 }
